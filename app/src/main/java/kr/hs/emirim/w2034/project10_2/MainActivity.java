7b414d91018c77db8ba4aa35a60deb77208231ae
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-20 09:18:17 +0900

    completion MainActivity (Project10_2)

--- app/src/main/java/kr/hs/emirim/w2034/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project10_2/MainActivity.java
@@ -3,12 +3,34 @@ package kr.hs.emirim.w2034.project10_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] imgNames = {"기생충", "겟아웃", "조커", "라라랜드", "뷰티인사이드", "겨울왕국2", "침입자", "크루엘라", "알라딘"};
+    int[] imgVIds = {R.id.img01, R.id.img02, R.id.img03, R.id.img04, R.id.img05, R.id.img06, R.id.img07, R.id.img08, R.id.img09};
+    int[] voteCount = new int[imgVIds.length];
+    ImageView[] image = new ImageView[imgVIds.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.title);
         setContentView(R.layout.activity_main);
+
+        for (int i=0; i < imgVIds.length; i++){
+            final int index;
+            index = i;
+            image[index] = findViewById(imgVIds[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imgNames[index]+": 총 "+voteCount[index]+"표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
