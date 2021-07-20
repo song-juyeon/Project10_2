3ea380b80fa7aa66f2e2aade5f281b29c09fd5bf
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-20 09:43:11 +0900

    completion MainActivity (Project10_2)

--- app/src/main/java/kr/hs/emirim/w2034/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project10_2/MainActivity.java
@@ -2,8 +2,10 @@ package kr.hs.emirim.w2034.project10_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -32,5 +34,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        Button btnFinish = findViewById(R.id.btn_done);
+        btnFinish.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("voteCount", voteCount);
+            intent.putExtra("imgNames", imgNames);
+            startActivity(intent);
+        }
+    };
 }
